d70dc1fcc4a39fd5eb95194f1e4d6e2b6b58b9ce
lead1974 <email2lead@gmail.com>
2020-06-02 11:04:47 -0700

    test

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace TMWorkRazorV2
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //test
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
